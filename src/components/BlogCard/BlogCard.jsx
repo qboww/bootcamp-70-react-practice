a1e0541909a2cc2e297641f9564656f925a973e4
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-04-25 14:54:22 +0300

    task-1

---
+++ src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,13 @@
+export const BlogCard = ({ name, avatar, poster, tag, title, description }) => {
+  return (
+    <div>
+      BlogCard
+      <h2>name {name}</h2>
+      <img src={avatar} alt={name} />
+      <p>tag{tag}</p>
+      <img src={poster} alt={tag} />
+      <p>title {title}</p>
+      <p>description {description}</p>
+    </div>
+  );
+};
