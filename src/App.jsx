31b7627640dcae288cbec87cc536efc3c669b4c1
Dimka863 <dlysachenko98@gmail.com>
2024-04-25 14:22:59 +0200

    task-2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BlogCard } from "./components/BlogCard/BlogCard";
+import { StatisticsList } from "./components/StatisticsList/StatisticsList";
 import article from "./data/article.json";
+import stats from "./data/stats.json";
 
 function App() {
   return (
     <>
       <BlogCard {...article} />
+      <StatisticsList stats={stats} title="Statistics" />
     </>
   );
 }
