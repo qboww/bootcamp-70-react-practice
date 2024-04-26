6019cb8fedba78de2ff4fbe425e8e2ba215be8f0
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-26 14:34:59 +0300

    1task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { BlogCard } from "./components/BlogCard/BlogCard";
-import { StatisticsList } from "./components/StatisticsList/StatisticsList";
-import article from "./data/article.json";
-import stats from "./data/stats.json";
+import { Container, Section, StatisticsList, BlogCard } from "components";
+import article from "data/article.json";
+import stats from "data/stats.json";
 
 function App() {
   return (
-    <>
-      <BlogCard {...article} />
-      <StatisticsList stats={stats} title="Statistics" />
-    </>
+    <Section>
+      <Container>
+        <BlogCard {...article} />
+        <StatisticsList stats={stats} title="Statistics" />
+      </Container>
+    </Section>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from "./Section/Section";
+export * from "./Container/Container";
+export * from "./StatisticsList/StatisticsList";
+export * from "./BlogCard/BlogCard";
