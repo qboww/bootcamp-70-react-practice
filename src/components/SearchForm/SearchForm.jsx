0494c0abecd0623502601c8ffe565a5bd77d727d
d1m2o3n4 <d1m2o3n4@gmail.com>
2024-05-03 14:47:17 +0300

    task1

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,35 @@
+import { Field, Form, Formik } from 'formik';
+import s from './SearchForm.module.css';
+import * as Yup from 'yup';
+import { IoIosCreate } from 'react-icons/io';
+
+const validationSchema = Yup.object().shape({
+  text: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+});
+
+export const SearchForm = ({ onSubmit }) => {
+  return (
+    <Formik
+      initialValues={{
+        text: '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={values => {
+        onSubmit(values);
+      }}
+    >
+      {({ errors, touched }) => {
+        return (
+          <Form className={s.searchForm}>
+            <Field name="text" className={s.searchInput} />
+            <button type="submit" className={s.searchBtn}>
+              <IoIosCreate />
+            </button>
+            {errors.text && touched.text ? <div className={s.errorMsg}>{errors.text}</div> : null}
+            {/* <ErrorMessage component="div" name="text" /> */}
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
--- src/tabs/Todos/Todos.jsx
+++ src/tabs/Todos/Todos.jsx
@@ -1,27 +1,32 @@
-import { Form, TodoList } from 'components';
+import { SearchForm, TodoList, Notification } from 'components';
 import { nanoid } from 'nanoid';
 
 import { useLocalStorage } from '../../hooks/useLocalStorage';
-import { Notification } from '../../components';
 
 export const Todos = () => {
   const [todos, setTodos] = useLocalStorage('todos', []);
 
-  const onSubmit = text => {
+  const onSubmit = ({ text }) => {
     const toDo = { text, id: nanoid() };
     setTodos([...todos, toDo]);
   };
 
-  const handleDelete = (todoId) => {
-    setTodos(todos.filter(item => {
-      return item.id !== todoId;
-    }));
+  const handleDelete = todoId => {
+    setTodos(
+      todos.filter(item => {
+        return item.id !== todoId;
+      })
+    );
   };
 
   return (
     <div>
-      <Form onSubmit={onSubmit} />
-      {todos.length ? <TodoList todos={todos} handleDelete={handleDelete} /> : <Notification text="All tasks are completed! 😉" />}
+      <SearchForm onSubmit={onSubmit} />
+      {todos.length ? (
+        <TodoList todos={todos} handleDelete={handleDelete} />
+      ) : (
+        <Notification text="All tasks are completed! 😉" />
+      )}
     </div>
   );
 };
