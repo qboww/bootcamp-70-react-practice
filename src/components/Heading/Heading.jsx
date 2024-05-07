0dca2cd1f586f057c4b5a9bab49757b8f8f76c5e
Dimka863 <dlysachenko98@gmail.com>
2024-05-07 13:36:40 +0200

    done

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,12 +1,13 @@
 import clsx from 'clsx';
 import s from './Heading.module.css';
-export const Heading = ({ title, top, bottom, info }) => {
+export const Heading = ({ title, top, bottom, info, error }) => {
   return (
     <h2
       className={clsx(s.title, {
         [s.top]: top,
         [s.bottom]: bottom,
         [s.info]: info,
+        [s.error]: error,
       })}
     >
       {title}
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,12 +2,16 @@ import { Field, Form, Formik } from 'formik';
 import s from './SearchForm.module.css';
 import * as Yup from 'yup';
 import { IoIosCreate } from 'react-icons/io';
+import { FaSearch } from 'react-icons/fa';
+import { MdAdsClick } from 'react-icons/md';
 
 const validationSchema = Yup.object().shape({
   text: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
 });
 
-export const SearchForm = ({ onSubmit }) => {
+const icons = { create: <IoIosCreate />, search: <FaSearch />, default: <MdAdsClick /> };
+
+export const SearchForm = ({ onSubmit, icon = 'default' }) => {
   return (
     <Formik
       initialValues={{
@@ -24,7 +28,7 @@ export const SearchForm = ({ onSubmit }) => {
           <Form className={s.searchForm}>
             <Field name="text" className={s.searchInput} />
             <button type="submit" className={s.searchBtn}>
-              <IoIosCreate />
+              {icons[icon]}
             </button>
             {errors.text && touched.text ? <div className={s.errorMsg}>{errors.text}</div> : null}
             {/* <ErrorMessage component="div" name="text" /> */}
